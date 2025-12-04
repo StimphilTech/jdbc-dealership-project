d6042fb211216a53690029800040caea7fb78632
Monalisa Stimphil <stimphiltechnologist@gmail.com>
2025-12-04 10:24:06 -0500

    Initial commit

---
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -0,0 +1,41 @@
+package com.yearup.dealership.db;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InventoryDao {
+    private DataSource dataSource;
+
+    public InventoryDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void addVehicleToInventory(String vin, int dealershipId) {
+        // TODO: Implement the logic to add a vehicle to the inventory
+    try (Connection connection = dataSource.getConnection();
+         PreparedStatement preparedStatement = connection.prepareStatement("insert into inventory (vin,dealershipId) values(?,?) "
+         ))
+    {preparedStatement.setString(1,vin);
+        preparedStatement.setInt(2,dealershipId);
+
+        int rows = preparedStatement.executeUpdate();
+        System.out.println("Rows Inserted: " + rows);
+
+    } catch (Exception e) {
+        e.printStackTrace();}}
+
+    public void removeVehicleFromInventory(String vin) {
+        // TODO: Implement the logic to remove a vehicle from the inventory
+        try Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("")) {
+         preparedStatement.setString(1,vin);
+
+            int rows = preparedStatement.executeUpdate();
+            System.out.println("Rows Deleted:" +rows);
+
+    }    catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
